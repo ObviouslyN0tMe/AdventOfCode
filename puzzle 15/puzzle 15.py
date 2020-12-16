72ed620f4b0745d2ac51589a5a2d47e07104c149
Josua Jeuck <josua@jeuck.de>
2020-12-16 13:12:57 +0100

    cleaned up puzzle 15

--- puzzle 15/puzzle 15.py
+++ puzzle 15/puzzle 15.py
@@ -6,18 +6,17 @@ def playGame(starting_numbers, game_lengh):
     mentioned_numbers = {}
     for index, value in enumerate(starting_numbers[:-1]):
         mentioned_numbers[value] = index + 1
-    starting_numbers.reverse()
     turn = len(starting_numbers)
-    starting_number = starting_numbers[0]
-    for i in range(turn + 1, game_lengh + 1, 1):
-        if starting_number not in mentioned_numbers.keys():
-            mentioned_numbers[starting_number] = i - 1
-            starting_number = 0
+    last_number = starting_numbers[-1]
+    for i in range(turn, game_lengh, 1):
+        if last_number not in mentioned_numbers.keys():
+            mentioned_numbers[last_number] = i
+            last_number = 0
         else:
-            new_number = i - 1 - mentioned_numbers[starting_number]
-            mentioned_numbers[starting_number] = i - 1
-            starting_number = new_number
-    return starting_number
+            new_number = i - mentioned_numbers[last_number]
+            mentioned_numbers[last_number] = i
+            last_number = new_number
+    return last_number
 
 
 print(playGame(rawdata, 30000000))
