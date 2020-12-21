b7c5047ddb4971ab063b871256a81984a808522f
Josua Jeuck <josua@jeuck.de>
2020-12-21 18:20:50 +0100

    removed food_list

--- puzzle 21/puzzle 21.py
+++ puzzle 21/puzzle 21.py
@@ -5,49 +5,44 @@ with open("puzzle input") as file:
 with open("test input") as file:
     testdata = [x.strip(")\n") for x in file.readlines()]
 
-food_list = {}
 all_ingredients = {}
-allergens_apperiance = {}
-ingredients_apperiance = {}
+allergens_appearance = {}
+ingredients_appearance = {}
 all_allergens = {}
+food_counters = {}
 
 
 # format food list
 def formatData(data):
-    global food_list, all_ingredients, allergens_apperiance, ingredients_apperiance
+    global food_list, all_ingredients, allergens_appearance, ingredients_appearance
     for line in data:
         line = line.split(" (contains ")
-        line_ingredients = line[0].split(" ")
+        line_ingredients = line[0].split()
         line_allergens = line[1].split(", ")
-        # save line in a dict
-        food_list[tuple(line_allergens)] = line_ingredients
         # save every ingredient with all lines it appears in and create a dict to save the matching allergen later
         for ingredient in line_ingredients:
-            if ingredient not in ingredients_apperiance:
+            if ingredient not in ingredients_appearance:
                 all_ingredients[ingredient] = []
-                ingredients_apperiance[ingredient] = [tuple(line_allergens)]
+                ingredients_appearance[ingredient] = [tuple(line_allergens)]
             else:
-                ingredients_apperiance[ingredient].append(tuple(line_allergens))
+                ingredients_appearance[ingredient].append(tuple(line_allergens))
         # save every allergen with all lines it appears in
         for allergen in line_allergens:
-            if allergen not in allergens_apperiance:
+            if allergen not in allergens_appearance:
                 all_allergens[allergen] = []
-                allergens_apperiance[allergen] = [tuple(line_allergens)]
+                allergens_appearance[allergen] = [line_ingredients]
+                food_counters[allergen] = 1
             else:
-                allergens_apperiance[allergen].append(tuple(line_allergens))
+                allergens_appearance[allergen] += line_ingredients
+                food_counters[allergen] += 1
 
 
 # remove all allergenfree ingredients
 def getAllergenFreeIngredients():
-    global food_list, all_ingredients, allergens_apperiance, ingredients_apperiance
-    for allergen, apperiance in allergens_apperiance.items():
-        food_counter = 0
-        possible_ing = []
-        for food in apperiance:
-            possible_ing += food_list[food]
-            food_counter += 1
+    global food_list, all_ingredients, allergens_appearance, ingredients_appearance
+    for allergen, possible_ing in allergens_appearance.items():
         for ingredient in possible_ing:
-            if possible_ing.count(ingredient) == food_counter:
+            if possible_ing.count(ingredient) == food_counters[allergen]:
                 if allergen not in all_ingredients[ingredient]:
                     all_ingredients[ingredient].append(allergen)
                 if ingredient not in all_allergens[allergen]:
@@ -56,7 +51,7 @@ def getAllergenFreeIngredients():
     to_pop = []
     for ingredient, possible_allergens in all_ingredients.items():
         if not possible_allergens:
-            allergen_free_counter += len(ingredients_apperiance[ingredient])
+            allergen_free_counter += len(ingredients_appearance[ingredient])
             to_pop.append(ingredient)
     for ingredient in to_pop:
         all_ingredients.pop(ingredient)
